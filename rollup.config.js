3f77b3c92fb064187c4f4a5f8494e02466590f1f
Rafael Gómez Bermejo <sernn2@gmail.com>
2021-12-08 12:26:53 +0100

    config inicial

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ if you want to view the source visit the plugins github repository
 export default {
   input: 'src/main.ts',
   output: {
-    dir: '.',
+    dir: 'obsidian-dbfolder',
     sourcemap: 'inline',
     sourcemapExcludeSources: isProd,
     format: 'cjs',
