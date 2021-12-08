78dc6b0ff89851fa320d099d98cfa7f8fedf7db5
Rafael Gómez Bermejo <sernn2@gmail.com>
2021-12-08 13:13:15 +0100

    devops local funcionando

---
+++ test/local-deploy.py
@@ -0,0 +1,22 @@
+import os
+import yaml
+import shutil
+'''
+read from config.yaml file the origin and target folders and copy the files inside
+'''
+if __name__ == '__main__':
+    with open('test/config.yaml', 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+            origin = config['base']['origin']
+            target = config['base']['target']
+            print("target path:"+target)
+            avaliableFiles = config['files']
+            for folder in origin:
+                print("Iterating files from path:"+folder)
+                for file in os.listdir(folder):
+                    if file.endswith(tuple(avaliableFiles)):
+                        print("Moving file: " + file)
+                        shutil.copy(folder+file, target+file)
+        except yaml.YAMLError as exc:
+            print(exc)
