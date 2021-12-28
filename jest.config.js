40edfbf39fa090ddb038d3c33a064b7e360bb616
Rafael Gómez Bermejo <sernn2@gmail.com>
2021-12-28 20:53:08 +0100

    probando..

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-    'transform': {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    'roots': [
-        '<rootDir>/src'
-    ],
-    'testRegex': "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    'testPathIgnorePatterns': ["/devops/", "/node_modules/"],
-    'moduleDirectories': ["node_modules", "src"],
-    'moduleFileExtensions': ["ts", "tsx", "js", "jsx", "json", "node"],
-    'collectCoverage': true
-}
+    preset: "ts-jest",
+    testEnvironment: "node",
+    modulePaths: ["node_modules", "<rootDir>/src"],
+  };
