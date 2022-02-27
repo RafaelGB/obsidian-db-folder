09df9d189194e6d1a6a51ef1e985fc21e54702ff
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-02-27 13:56:29 +0100

    quitando logs

--- src/cross/VaultManagement.ts
+++ src/cross/VaultManagement.ts
@@ -2,7 +2,6 @@ import { App } from "obsidian";
 import { TableRows } from 'cdm/FolderModel';
 export function obtainCurrentFolder(app: App): string {
     const file = app.workspace.getActiveFile();
-    console.log("file",file);
     // obtain folder to check
     if(!file){
         return null;
@@ -14,7 +13,6 @@ export function adapterTFilesToRows(app: App, folderPath: string): TableRows {
     const rows: TableRows = [];
     // TODO improve this filter?
     let id = 0;
-    console.log("folderPath",folderPath);
     app.vault.getFiles().forEach(file => {
         if(file.path.startsWith(folderPath)){
             const aFile = {
@@ -24,6 +22,5 @@ export function adapterTFilesToRows(app: App, folderPath: string): TableRows {
             rows.push(aFile);
         }
     });
-    console.log("adapter rows",rows);
     return rows;
 }
