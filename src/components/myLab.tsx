7f3a6d6f939fa52d8e957090d8f1e8659e46e9c6
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-04-02 18:58:03 +0200

    clear

--- src/components/myLab.tsx
+++ src/components/myLab.tsx
@@ -1,15 +1,7 @@
 import { App} from "obsidian";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import {DatabaseContext } from 'context/context';
 
-const useApp = (): App | undefined => {
+export const useApp = (): App | undefined => {
     return React.useContext(DatabaseContext);
   };
-
-const ReactView = () => {
-    const { vault } = useApp();
-  
-    return <h4>{vault.getName()}</h4>;
-};
-
