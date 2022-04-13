85a2884d09d28a1e4ed9faa0bf3529256120b818
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-04-13 12:50:24 +0200

    modular mas

--- src/services/FileManagerService.ts
+++ src/services/FileManagerService.ts
@@ -1,85 +1,7 @@
+import { FileContent } from "helpers/FileContent";
 import { obtainContentFromTfile, obtainTfileFromFilePath } from "helpers/VaultManagement";
 import { TFile, TFolder } from "obsidian";
-import { LOGGER } from "./Logger";
-
-class FileContent{
-    public value: string;
-    constructor(string: string){
-        this.value = string;
-    }
-
-    object(): string[]{
-        return this.real_array(this.value.split('\n'));
-    }
-
-    replaceAll(pattern_to_replace:any, input:any): FileContent{
-      if(input!==''){
-        if(Array.isArray(pattern_to_replace)){
-          pattern_to_replace.forEach(
-            (regex,index) =>  {
-              this.value = this.value.replaceAll(
-                regex,
-                input[index]
-              );
-            }
-          );
-        }else{
-          this.value = this.value.replaceAll(
-            pattern_to_replace,
-            input
-          );
-        }
-      }
-      return this;
-    }
-
-    remove(pattern_to_be_removed:any): FileContent{
-        let _object = this.object();
-        _object.forEach((value,index) => {
-            if (value.match(pattern_to_be_removed)){
-                delete _object[index];
-            }
-        });
-        this.value = this.real_array(_object).join('\n');
-        return this;
-    }
-
-    removeAll(string_to_be_removed:any): FileContent{
-        let _object = this.object();
-        _object.forEach((value,index) => {
-            if (value.trim().indexOf(string_to_be_removed)!=-1){
-                delete _object[index];
-            }
-        });
-        this.value = this.real_array(_object).join('\n');
-        return this;
-    }
-
-    fetch(line_number:number){
-        let _object = this.object();
-        for (let i=0;i<_object.length;i++){
-            if (i+1===line_number){
-                return _object[i];
-            }
-        }
-        return null;
-    }
-
-    edit(content:string,line_number:any): FileContent{
-         let _object = this.object();
-        _object[line_number-1] = content;
-         this.value = this.real_array(_object).join('\n');
-         return this;
-     }
-
-    real_array(array:string[]):string[]{
-        let output:string[] = [];
-        array.forEach(element=>{
-            output.push(element);
-        });
-        return output;
-    }
-}
+import { LOGGER } from "services/Logger";
 export class FileManager{
     private static instance: FileManager;
     constructor(){}
@@ -100,7 +22,10 @@ export class FileManager{
         LOGGER.debug(`<= create_markdown_file`);
         return created_note;
     }
-
+     /**
+      * Edit file content
+      * @param note 
+      */
     async editNoteContent(note:any) {
         LOGGER.debug(`=> editNoteContent. note:${JSON.stringify(note)}`);
         try{
