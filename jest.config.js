acc7a4d15f4877b933a24f2d37793885220ac2d8
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-04-24 02:56:01 +0200

    divaguing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  rootDir: ".",
+  roots: [
+    "<rootDir>",
+    "<rootDir>/src/",
+    "<rootDir>/node_modules/"
+  ],
+  modulePaths: [
+    "<rootDir>",
+    "<rootDir>/src",
+    "<rootDir>/node_modules"
+  ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   moduleDirectories: ["node_modules", "src"],
 };
