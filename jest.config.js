b88a5f50aaea32168ba943245feeab8c5acb0557
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-04-24 03:28:48 +0200

    new libraries to future tests. right now does not work yet

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,20 @@ module.exports = {
     "<rootDir>/src",
     "<rootDir>/node_modules"
   ],
+  moduleNameMapper: {
+    "src/(.*)": "<rootDir>/src/$1",
+    "node_modules/(.*)": "<rootDir>/node_modules/$1",
+    "obsidian-dataview": "<rootDir>/node_modules/obsidian-dataview/lib/index.js"
+  },
+  transform: {
+    "^.+\\.svelte$": [
+    "svelte-jester",
+    {
+      "preprocess": true
+    }
+    ],
+    "^.+\\.ts$": "ts-jest"
+  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   moduleDirectories: ["node_modules", "src"],
 };
