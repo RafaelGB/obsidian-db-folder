1306a6458075c7c8c9a44e48a0d7549c9d4b8e76
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-04-29 10:19:14 +0200

    moduling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { usePopper } from "react-popper";
-import { grey, randomColor } from "helpers/Colors";
-import ArrowUpIcon from "components/img/ArrowUp";
-import ArrowDownIcon from "components/img/ArrowDown";
-import ArrowLeftIcon from "components/img/ArrowLeft";
-import ArrowRightIcon from "components/img/ArrowRight";
-import TrashIcon from "components/img/Trash";
+import React, { useState } from "react";
+import { randomColor } from "helpers/Colors";
 import TextIcon from "components/img/Text";
 import MultiIcon from "components/img/Multi";
 import HashIcon from "components/img/Hash";
 import PlusIcon from "components/img/Plus";
 import MarkdownObsidian from "components/img/Markdown";
-import {
-  ActionTypes,
-  DataTypes,
-  MetadataColumns,
-  StyleVariables,
-} from "helpers/Constants";
+import { ActionTypes, DataTypes, MetadataColumns } from "helpers/Constants";
 import { LOGGER } from "services/Logger";
 import { DatabaseHeaderProps } from "cdm/FolderModel";
 import ReactDOM from "react-dom";
-import { dbTrim } from "helpers/StylesHelper";
+import HeaderMenu from "./HeaderMenu";
 
 function setOptionsOfSelectDataType(
   options: any[],
@@ -51,7 +40,7 @@ export default function Header(headerProps: DatabaseHeaderProps) {
   /** Properties of header */
   const { setSortBy, rows } = headerProps;
   /** Column values */
-  const { id, key, dataType, options, getResizerProps } = headerProps.column;
+  const { id, dataType, options, getResizerProps } = headerProps.column;
   /** reducer asociated to database */
   // TODO typying improve
   const dataDispatch = (headerProps as any).dataDispatch;
@@ -59,118 +48,11 @@ export default function Header(headerProps: DatabaseHeaderProps) {
   const [domReady, setDomReady] = useState(false);
   const [referenceElement, setReferenceElement] = useState(null);
   const [isMetadata, setIsMetadata] = useState(headerProps.column.isMetadata);
-  const [popperElement, setPopperElement] = useState(null);
-  const [inputRef, setInputRef] = useState(null);
-  const { styles, attributes } = usePopper(referenceElement, popperElement, {
-    placement: "bottom",
-    strategy: "absolute",
-  });
   const [labelState, setLabelState] = useState(headerProps.column.label);
-  const [keyState, setkeyState] = useState(dbTrim(key));
-  const [typeReferenceElement, setTypeReferenceElement] = useState(null);
-  const [typePopperElement, setTypePopperElement] = useState(null);
-  const [showType, setShowType] = useState(false);
 
   React.useEffect(() => {
     setDomReady(true);
   });
-
-  const buttons = [
-    {
-      onClick: (e: any) => {
-        setSortBy([{ id: id, desc: false }]);
-        setExpanded(false);
-      },
-      icon: <ArrowUpIcon />,
-      label: "Sort ascending",
-    },
-    {
-      onClick: (e: any) => {
-        setSortBy([{ id: id, desc: true }]);
-        setExpanded(false);
-      },
-      icon: <ArrowDownIcon />,
-      label: "Sort descending",
-    },
-    {
-      onClick: (e: any) => {
-        dataDispatch({
-          type: ActionTypes.ADD_COLUMN_TO_LEFT,
-          columnId: id,
-          focus: false,
-        });
-        setExpanded(false);
-      },
-      icon: <ArrowLeftIcon />,
-      label: "Insert left",
-    },
-    {
-      onClick: (e: any) => {
-        dataDispatch({
-          type: ActionTypes.ADD_COLUMN_TO_RIGHT,
-          columnId: id,
-          focus: false,
-        });
-        setExpanded(false);
-      },
-      icon: <ArrowRightIcon />,
-      label: "Insert right",
-    },
-    {
-      onClick: (e: any) => {
-        dataDispatch({
-          type: ActionTypes.DELETE_COLUMN,
-          columnId: id,
-          key: keyState,
-        });
-        setExpanded(false);
-      },
-      icon: <TrashIcon />,
-      label: "Delete",
-    },
-  ];
-
-  const types = [
-    {
-      onClick: (e: any) => {
-        dataDispatch({
-          type: ActionTypes.UPDATE_COLUMN_TYPE,
-          columnId: id,
-          dataType: DataTypes.SELECT,
-        });
-        setShowType(false);
-        setExpanded(false);
-      },
-      icon: <MultiIcon />,
-      label: DataTypes.SELECT,
-    },
-    {
-      onClick: (e: any) => {
-        dataDispatch({
-          type: ActionTypes.UPDATE_COLUMN_TYPE,
-          columnId: id,
-          dataType: DataTypes.TEXT,
-        });
-        setShowType(false);
-        setExpanded(false);
-      },
-      icon: <TextIcon />,
-      label: DataTypes.TEXT,
-    },
-    {
-      onClick: (e: any) => {
-        dataDispatch({
-          type: ActionTypes.UPDATE_COLUMN_TYPE,
-          columnId: id,
-          dataType: DataTypes.NUMBER,
-        });
-        setShowType(false);
-        setExpanded(false);
-      },
-      icon: <HashIcon />,
-      label: DataTypes.NUMBER,
-    },
-  ];
 
   let propertyIcon: any;
   switch (dataType) {
@@ -192,44 +74,6 @@ export default function Header(headerProps: DatabaseHeaderProps) {
       break;
   }
 
-  useEffect(() => {
-    if (created) {
-      setExpanded(true);
-    }
-  }, [created]);
-
-  useEffect(() => {
-    setLabelState(labelState);
-  }, [labelState]);
-
-  useEffect(() => {
-    if (inputRef) {
-      inputRef.focus();
-      inputRef.select();
-    }
-  }, [inputRef]);
-  const typePopper = usePopper(typeReferenceElement, typePopperElement, {
-    placement: "right",
-    strategy: "fixed",
-  });
-
-  function handleKeyDown(e: any) {
-    if (e.key === "Enter") {
-      dataDispatch({
-        type: ActionTypes.UPDATE_COLUMN_LABEL,
-        columnId: id,
-        accessor: keyState,
-        label: labelState,
-      });
-      setExpanded(false);
-      setkeyState(dbTrim(labelState));
-    }
-  }
-
-  function handleChange(e: any) {
-    setLabelState(e.target.value);
-  }
-
   function handlerAddColumnToLeft(e: any) {
     dataDispatch({
       type: ActionTypes.ADD_COLUMN_TO_LEFT,
@@ -238,134 +82,6 @@ export default function Header(headerProps: DatabaseHeaderProps) {
     });
   }
 
-  /**
-   * When user leaves the input field
-   * @param e
-   */
-  function handleBlur(e: any) {
-    e.preventDefault();
-    //dataDispatch({type: ActionTypes.UPDATE_COLUMN_LABEL, columnId: id, label: header});
-  }
-
-  function renderHeaderOptions() {
-    return (
-      <div>
-        {expanded && (
-          <div className="overlay" onClick={() => setExpanded(false)} />
-        )}
-        {expanded && (
-          <div
-            ref={setPopperElement}
-            style={{ ...styles.popper, zIndex: 3 }}
-            {...attributes.popper}
-          >
-            <div
-              className="shadow-5 border-radius-md"
-              style={{
-                width: 240,
-                backgroundColor: StyleVariables.BACKGROUND_SECONDARY,
-              }}
-            >
-              {/** Edit header label section */}
-              <div
-                style={{
-                  paddingTop: "0.75rem",
-                  paddingLeft: "0.75rem",
-                  paddingRight: "0.75rem",
-                }}
-              >
-                <div className="is-fullwidth" style={{ marginBottom: 12 }}>
-                  <input
-                    className="form-input"
-                    ref={setInputRef}
-                    type="text"
-                    value={labelState}
-                    style={{ width: "100%" }}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    onKeyDown={handleKeyDown}
-                  />
-                </div>
-                <span
-                  className="font-weight-600 font-size-75"
-                  style={{
-                    textTransform: "uppercase",
-                    color: StyleVariables.TEXT_FAINT,
-                  }}
-                >
-                  Property Type
-                </span>
-              </div>
-              {/** Edit header label section */}
-              <div style={{ padding: "4px 0px" }}>
-                <button
-                  className="sort-button"
-                  type="button"
-                  onMouseEnter={() => setShowType(true)}
-                  onMouseLeave={() => setShowType(false)}
-                  ref={setTypeReferenceElement}
-                >
-                  <span className="svg-icon svg-text icon-margin">
-                    {propertyIcon}
-                  </span>
-                  <span style={{ textTransform: "capitalize" }}>
-                    {dataType}
-                  </span>
-                </button>
-                {showType && (
-                  <div
-                    className="shadow-5 border-radius-m"
-                    ref={setTypePopperElement}
-                    onMouseEnter={() => setShowType(true)}
-                    onMouseLeave={() => setShowType(false)}
-                    {...typePopper.attributes.popper}
-                    style={{
-                      ...typePopper.styles.popper,
-                      width: 200,
-                      backgroundColor: StyleVariables.BACKGROUND_SECONDARY,
-                      zIndex: 4,
-                      padding: "4px 0px",
-                    }}
-                  >
-                    {types.map((type) => (
-                      <div key={type.label}>
-                        <button className="sort-button" onClick={type.onClick}>
-                          <span className="svg-icon svg-text icon-margin">
-                            {type.icon}
-                          </span>
-                          {type.label}
-                        </button>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-              <div
-                style={{
-                  borderTop: `2px solid ${StyleVariables.BACKGROUND_DIVIDER}`,
-                  padding: "4px 0px",
-                }}
-              >
-                {buttons.map((button) => (
-                  <button
-                    key={button.label}
-                    type="button"
-                    className="sort-button"
-                    onMouseDown={button.onClick}
-                  >
-                    <span className="svg-icon svg-text icon-margin">
-                      {button.icon}
-                    </span>
-                    {button.label}
-                  </button>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
   LOGGER.debug(`<=Header ${headerProps.column.label}`);
   return id !== MetadataColumns.ADD_COLUMN ? (
     <>
@@ -380,7 +96,18 @@ export default function Header(headerProps: DatabaseHeaderProps) {
       <div {...getResizerProps()} className="resizer" />
       {!isMetadata && domReady
         ? ReactDOM.createPortal(
-            renderHeaderOptions(),
+            <HeaderMenu
+              column={headerProps.column}
+              dispatch={dataDispatch}
+              setSortBy={setSortBy}
+              propertyIcon={propertyIcon}
+              expanded={expanded}
+              setExpanded={setExpanded}
+              created={created}
+              referenceElement={referenceElement}
+              labelState={labelState}
+              setLabelState={setLabelState}
+            />,
             document.getElementById("popper-container")
           )
         : null}
