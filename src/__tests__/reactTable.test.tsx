42e2d3107d0af62c042a97b8f68787bad2681f2a
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-05-05 19:19:41 +0200

    now that app is mocked, render works! but mockedTableData must be improved

--- src/__tests__/reactTable.test.tsx
+++ src/__tests__/reactTable.test.tsx
@@ -1,15 +1,17 @@
 import { render, screen } from "@testing-library/react";
-import obsidian from "obsidian";
 import { Database } from "components/index/Database";
 import { TableDataType } from "cdm/FolderModel";
 import { makeData } from "mock/mockUtils";
 import React from "react";
 
-// jest.mock("obsidian");
-
-test("Render without crashing", () => {
-  //   const mockedTableData: TableDataType = makeData(10);
-  //   render(<Database {...mockedTableData} />);
-  //   const titleLabel = screen.getByText(" title");
-  //   expect(titleLabel).toBeInTheDocument();
+/**
+ * @jest-environment node || jsdom
+ */
+describe("React-table", () => {
+  test("Render without crashing", () => {
+    const mockedTableData: TableDataType = makeData(10);
+    render(<Database {...mockedTableData} />);
+    const titleLabel = screen.getByText(" title");
+    expect(titleLabel).toBeInTheDocument();
+  });
 });
