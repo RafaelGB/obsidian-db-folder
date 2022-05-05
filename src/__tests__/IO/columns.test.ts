15d8a47b99deb402c64522c9bd517c5993cf0783
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-05-05 19:10:21 +0200

    test columns

--- src/__tests__/IO/columns.test.ts
+++ src/__tests__/IO/columns.test.ts
@@ -1,23 +1,18 @@
-import { DatabaseColumn } from "cdm/DatabaseModel";
-import { obtainMetadataColumns } from "components/Columns";
-import { DataTypes, MetadataColumns } from "helpers/Constants";
-
-test('Obtain metadata columns test', async () => {
-    const result = await obtainMetadataColumns(generateYamlColumns(5));
-    expect(result).toHaveProperty(MetadataColumns.FILE);
-});
-
-const generateYamlColumns = (length: number): Record<string, DatabaseColumn> => {
-    const yamlColumns: Record<string, DatabaseColumn> = {};
-    yamlColumns['column1'] = {
-        input: DataTypes.TEXT,
-        accessor: "uniqueKey",
-        label: "custom label",
-        key: "uniqueKey",
-        position: 1,
-        isMetadata: false,
-        skipPersist: true,
-        csvCandidate: false
-    }
-    return yamlColumns;
-}
+import { obtainMetadataColumns, obtainColumnsFromFolder } from "components/Columns";
+import { MetadataColumns } from "helpers/Constants";
+import { generateYamlColumns } from "mock/mockUtils";
+describe("Columns", () => {
+    /** Metadata columns */
+    test('obtainMetadataColumns()', async () => {
+        // Check if mandatory columns exists
+        const columnsRecord = await obtainMetadataColumns(generateYamlColumns(5));
+        expect(columnsRecord).toHaveProperty(MetadataColumns.FILE);
+        expect(columnsRecord).toHaveProperty(MetadataColumns.ADD_COLUMN);
+    });
+    /** Table columns */
+    test('obtainColumnsFromFolder()', async () => {
+        const columnsRecord = await obtainMetadataColumns(generateYamlColumns(5));
+        const tableColumns = await obtainColumnsFromFolder(columnsRecord);
+        expect(tableColumns.length >= 5).toBeTruthy();
+    });
+}); 
