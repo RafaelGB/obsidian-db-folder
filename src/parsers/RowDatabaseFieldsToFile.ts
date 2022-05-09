a358636613983733b64c8d3d143fefb8eb18e403
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-05-09 12:45:00 +0200

    mandatory

--- src/parsers/RowDatabaseFieldsToFile.ts
+++ src/parsers/RowDatabaseFieldsToFile.ts
@@ -1,6 +1,6 @@
 import { RowDatabaseFields } from "cdm/DatabaseModel";
 import { parseYaml } from "obsidian";
-export const parseFrontmatterFieldsToString = (databaseFields: RowDatabaseFields, original?: string, deletedColumn?: string): string => {
+export const parseFrontmatterFieldsToString = (databaseFields: RowDatabaseFields, frontmatterGroupRegex: RegExp, original: string, deletedColumn?: string): string => {
     const frontmatterFields = databaseFields.frontmatter;
     const inlineFields = databaseFields.inline;
     const array: string[] = [];
@@ -8,20 +8,20 @@ export const parseFrontmatterFieldsToString = (databaseFields: RowDatabaseFields
     Object.keys(frontmatterFields).forEach(key => {
         array.push(`${key}: ${frontmatterFields[key]}`);
     });
-    if (original !== undefined) {
-        const match = original.match(/^---\s+([\w\W]+?)\s+---/);
-        if (match) {
-            const frontmatterRaw = match[1];
-            const yaml = parseYaml(frontmatterRaw);
-            Object.keys(yaml).forEach(key => {
-                // add frontmatter fields that are not specified as database fields
-                // check if frontmatter field is inside inline fields
-                if (!inlineFields.hasOwnProperty(key) && !frontmatterFields[key] && key !== deletedColumn) {
-                    array.push(`${key}: ${yaml[key] ?? ''}`);
-                }
-            });
-        }
+
+    const match = original.match(frontmatterGroupRegex);
+    if (match) {
+        const frontmatterRaw = match[1];
+        const yaml = parseYaml(frontmatterRaw);
+        Object.keys(yaml).forEach(key => {
+            // add frontmatter fields that are not specified as database fields
+            // check if frontmatter field is inside inline fields
+            if (!inlineFields.hasOwnProperty(key) && !frontmatterFields[key] && key !== deletedColumn) {
+                array.push(`${key}: ${yaml[key] ?? ''}`);
+            }
+        });
     }
+
     array.push(`---`);
     return array.join('\n');
 }
