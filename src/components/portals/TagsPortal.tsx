c3ed863895a3cb0e2ab9e195bcf7a7845d3547b3
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-06-05 19:20:12 +0200

    alternative to portal

--- src/components/portals/TagsPortal.tsx
+++ src/components/portals/TagsPortal.tsx
@@ -1,31 +1,23 @@
 import { RowSelectOption, TagsProps } from "cdm/ComponentsModel";
 import Relationship from "components/RelationShip";
-import Select, { StylesConfig } from "react-select";
+import CreatableSelect from "react-select/creatable";
+
 import { grey } from "helpers/Colors";
 import React, { useState } from "react";
 import { usePopper } from "react-popper";
 import ReactDOM from "react-dom";
 import { StyleVariables } from "helpers/Constants";
+import { ActionMeta, OnChangeValue } from "react-select";
 
 const TagsPortal = (tagsProps: TagsProps) => {
   const { intialState, column, dispatch, cellProperties } = tagsProps;
   const { row } = cellProperties;
   // Tags reference state
   const [showSelectTags, setShowSelectTags] = useState(false);
-  const [tagsRef, setTagsRef] = useState(null);
-  // Selector popper state
-  const [tagsPop, setTagsPop] = useState(null);
-  const { styles, attributes } = usePopper(tagsRef, tagsPop);
-  // Selector popper state
-  const [domReady, setDomReady] = useState(false);
   // tags values state
   const [tagsState, setTagsState] = useState(
     intialState.data[row.index][column.key]
   );
-
-  React.useEffect(() => {
-    setDomReady(true);
-  });
 
   function getColor() {
     const match = column.options.find(
@@ -38,50 +30,54 @@ const TagsPortal = (tagsProps: TagsProps) => {
     label: option.label,
     color: option.backgroundColor,
   }));
+
+  const handleOnChange = (
+    newValue: OnChangeValue<any, true>,
+    actionMeta: ActionMeta<RowSelectOption>
+  ) => {
+    console.group("Value Changed");
+    console.log(newValue);
+    console.log(`action: ${actionMeta.action}`);
+    console.groupEnd();
+  };
+
   function PortalTags() {
     return (
       <div
         className="menu"
-        ref={setTagsPop}
-        {...attributes.popper}
         style={{
-          ...styles.popper,
           padding: "0.75rem",
           background: StyleVariables.BACKGROUND_SECONDARY,
-          overflow: "visible",
+          zIndex: 4,
+          minWidth: 200,
+          maxWidth: 320,
         }}
       >
-        {showSelectTags && (
-          <Select
-            closeMenuOnSelect={false}
-            isMulti
-            options={multiOptions}
-            onBlur={() => setShowSelectTags(false)}
-          />
-        )}
+        <CreatableSelect
+          closeMenuOnSelect={false}
+          isMulti
+          options={multiOptions}
+          onBlur={() => setShowSelectTags(false)}
+          onChange={handleOnChange}
+        />
       </div>
     );
   }
   return (
     <>
-      <div
-        ref={setTagsRef}
-        className="cell-padding d-flex cursor-default align-items-center flex-1"
-        onClick={() => setShowSelectTags(!showSelectTags)}
-      >
-        {tagsState && (
-          <Relationship
-            value={tagsState.toString()}
-            backgroundColor={getColor()}
-          />
-        )}
-      </div>
-      {domReady && showSelectTags
-        ? ReactDOM.createPortal(
-            PortalTags(),
-            document.getElementById("popper-container")
-          )
-        : null}
+      {showSelectTags
+        ? PortalTags()
+        : tagsState && (
+            <div
+              className="cell-padding d-flex cursor-default align-items-center flex-1"
+              onClick={() => setShowSelectTags(true)}
+            >
+              <Relationship
+                value={tagsState.toString()}
+                backgroundColor={getColor()}
+              />
+            </div>
+          )}
     </>
   );
 };
