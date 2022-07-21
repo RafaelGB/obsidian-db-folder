4f9abb0e6ec7bd76be84283dcb3e218c96788b89
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-07-21 14:41:29 +0200

    fix config yaml read

--- src/parsers/handlers/unmarshall/UnmarshallConfigHandler.ts
+++ src/parsers/handlers/unmarshall/UnmarshallConfigHandler.ts
@@ -16,11 +16,11 @@ export class UnmarshallConfigHandler extends AbstractDiskHandler {
                 this.localDisk.push(`${YAML_INDENT.repeat(1)}${key}:`);
                 Object.entries(valueConfig).forEach(([key, valueInternal]) => {
                     // Lvl3: config properties
-                    this.localDisk.push(`${YAML_INDENT.repeat(2)}${key}: ${parseValue(valueInternal as string, config)}`);
+                    this.localDisk.push(`${YAML_INDENT.repeat(2)}${key}: "${parseValue(valueInternal as string, config)}"`);
                 });
             } else {
                 // Lvl2: config properties
-                this.localDisk.push(`${YAML_INDENT.repeat(1)}${key}: ${parseValue(valueConfig, config)}`);
+                this.localDisk.push(`${YAML_INDENT.repeat(1)}${key}: "${parseValue(valueConfig, config)}"`);
             }
         });
         return this.goNext(handlerResponse);
