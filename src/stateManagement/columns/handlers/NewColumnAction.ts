d4172877e3d8d16f74c377131bba47bb312991fe
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-08-04 18:47:11 +0200

    PoC of chain is working correctly

--- src/stateManagement/columns/handlers/NewColumnAction.ts
+++ src/stateManagement/columns/handlers/NewColumnAction.ts
@@ -1,88 +1,78 @@
 import { DatabaseColumn } from "cdm/DatabaseModel";
 import { TableColumn } from "cdm/FolderModel";
 import { ColumnsState, TableActionResponse } from "cdm/TableStateInterface";
+import { DatabaseView } from "DatabaseView";
 import { DEFAULT_COLUMN_CONFIG, InputType, TableColumnsTemplate } from "helpers/Constants";
 import { AbstractTableAction } from "stateManagement/AbstractTableAction";
 
 export default class InsertColumnHandlerAction extends AbstractTableAction<ColumnsState> {
     handle(tableActionResponse: TableActionResponse<ColumnsState>): TableActionResponse<ColumnsState> {
         const { view, set, implementation } = tableActionResponse;
+
         implementation.addToLeft = (column: TableColumn) =>
             set((updater) => {
-                const columnInfo = this.generateNewColumnInfo(
-                    column.position - 1,
-                    updater.columns,
-                    updater.shadowColumns
-                );
-                const newLeftColumn: DatabaseColumn = {
-                    input: InputType.TEXT,
-                    accessorKey: columnInfo.name,
-                    key: columnInfo.name,
-                    label: columnInfo.label,
-                    position: columnInfo.position,
-                    config: DEFAULT_COLUMN_CONFIG,
-                };
-                view.diskConfig.addColumn(columnInfo.name, newLeftColumn);
-                const leftIndex = updater.columns.findIndex(
+                const index = updater.columns.findIndex(
                     (col) => col.id === column.id
                 );
-                const newColumns = [
-                    ...updater.columns.slice(0, leftIndex),
-                    {
-                        ...TableColumnsTemplate,
-                        input: newLeftColumn.input,
-                        id: newLeftColumn.key,
-                        label: newLeftColumn.label,
-                        key: newLeftColumn.key,
-                        accessorKey: newLeftColumn.accessorKey,
-                        position: newLeftColumn.position,
-                        csvCandidate: true,
-                        config: newLeftColumn.config,
-                    },
-                    ...updater.columns.slice(leftIndex, updater.columns.length),
-                ];
-                return { columns: newColumns };
+                const alteredColumns = this.generateNewColumn(view, updater, index, column.position - 1);
+                return { columns: alteredColumns };
             });
+
         implementation.addToRight = (column: TableColumn) =>
             set((updater) => {
-                const columnInfo = this.generateNewColumnInfo(
-                    column.position + 1,
-                    updater.columns,
-                    updater.shadowColumns
-                );
-                const newLeftColumn: DatabaseColumn = {
-                    input: InputType.TEXT,
-                    accessorKey: columnInfo.name,
-                    key: columnInfo.name,
-                    label: columnInfo.label,
-                    position: columnInfo.position,
-                    config: DEFAULT_COLUMN_CONFIG,
-                };
-                view.diskConfig.addColumn(columnInfo.name, newLeftColumn);
-                const rigthIndex = updater.columns.findIndex(
+                const index = updater.columns.findIndex(
                     (col) => col.id === column.id
                 );
-                const newColumns = [
-                    ...updater.columns.slice(0, rigthIndex + 1),
-                    {
-                        ...TableColumnsTemplate,
-                        input: newLeftColumn.input,
-                        id: newLeftColumn.key,
-                        label: newLeftColumn.label,
-                        key: newLeftColumn.key,
-                        accessorKey: newLeftColumn.accessorKey,
-                        position: newLeftColumn.position,
-                        csvCandidate: true,
-                        config: newLeftColumn.config,
-                    },
-                    ...updater.columns.slice(rigthIndex + 1, updater.columns.length),
-                ];
-                return { columns: newColumns };
+                const alteredColumns = this.generateNewColumn(view, updater, index + 1, column.position + 1);
+                return { columns: alteredColumns };
             });
 
         tableActionResponse.implementation = implementation;
         return this.goNext(tableActionResponse);
     }
+
+    /**
+     * Generate new column in function of the position
+     * @param view 
+     * @param updater 
+     * @param index 
+     * @param desiredPosition 
+     * @returns 
+     */
+    private generateNewColumn(view: DatabaseView, updater: ColumnsState, index: number, desiredPosition: number): TableColumn[] {
+        const columnInfo = this.generateNewColumnInfo(
+            desiredPosition,
+            updater.columns,
+            updater.shadowColumns
+        );
+        const newLeftColumn: DatabaseColumn = {
+            input: InputType.TEXT,
+            accessorKey: columnInfo.name,
+            key: columnInfo.name,
+            label: columnInfo.label,
+            position: columnInfo.position,
+            config: DEFAULT_COLUMN_CONFIG,
+        };
+        view.diskConfig.addColumn(columnInfo.name, newLeftColumn);
+
+        const newColumns = [
+            ...updater.columns.slice(0, index),
+            {
+                ...TableColumnsTemplate,
+                input: newLeftColumn.input,
+                id: newLeftColumn.key,
+                label: newLeftColumn.label,
+                key: newLeftColumn.key,
+                accessorKey: newLeftColumn.accessorKey,
+                position: newLeftColumn.position,
+                csvCandidate: true,
+                config: newLeftColumn.config,
+            },
+            ...updater.columns.slice(index, updater.columns.length),
+        ];
+        return newColumns;
+    }
+
     /**
      * Adjust width of the columns when add a new column.
      * @param wantedPosition
