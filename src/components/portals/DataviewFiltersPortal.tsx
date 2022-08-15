1947661b1c8ea94ec0c6c2c0ce31e3fd945e0f66
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-08-15 10:45:47 +0200

    data updates when modify operator or field

--- src/components/portals/DataviewFiltersPortal.tsx
+++ src/components/portals/DataviewFiltersPortal.tsx
@@ -5,10 +5,11 @@ import FormControl from "@mui/material/FormControl";
 import Grid from "@mui/material/Grid";
 
 import Input from "@mui/material/Input";
-import Select from "@mui/material/Select";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { DataviewFiltersProps } from "cdm/ComponentsModel";
 import { DatabaseColumn } from "cdm/DatabaseModel";
 import { obtainColumnsFromRows } from "components/Columns";
+import CrossIcon from "components/img/CrossIcon";
 import MenuDownIcon from "components/img/MenuDownIcon";
 import { OperatorFilter, StyleVariables } from "helpers/Constants";
 import React, { useState } from "react";
@@ -21,7 +22,9 @@ const DataviewFiltersPortal = (props: DataviewFiltersProps) => {
   const [ddbbConfig, filters, configActions] = tableState.configState(
     (state) => [state.ddbbConfig, state.filters, state.actions]
   );
+
   const columns = tableState.columns((state) => state.columns);
+  const dataActions = tableState.data((state) => state.actions);
   const [filtersRef, setFiltersRef] = useState(null);
   const [showFilters, setShowFilters] = useState(false);
 
@@ -35,69 +38,98 @@ const DataviewFiltersPortal = (props: DataviewFiltersProps) => {
 
   React.useEffect(() => {
     new Promise<Record<string, DatabaseColumn>>((resolve, reject) => {
-      resolve(obtainColumnsFromRows(view, ddbbConfig, filters, columns));
+      // Empty conditions to refresh the dataview
+      const emptyFilterConditions = { ...filters };
+      emptyFilterConditions.conditions = [];
+      resolve(
+        obtainColumnsFromRows(view, ddbbConfig, emptyFilterConditions, columns)
+      );
     }).then((columns) => {
-      console.log("Possible columns", columns);
       setPossibleColumns(Object.keys(columns));
     });
-  }, [ddbbConfig, filters, columns]);
+  }, [ddbbConfig, columns]);
 
   React.useEffect(() => {
     if (!domReady) {
       setDomReady(true);
     }
   });
+
+  const onchangeExistedColumnHandler =
+    (existedColumnIndex: number) =>
+    (event: SelectChangeEvent<string>, child: React.ReactNode) => {
+      const alteredFilterState = { ...filters };
+      alteredFilterState.conditions[existedColumnIndex].field =
+        event.target.value;
+      configActions.alterFilters(alteredFilterState);
+      dataActions.dataviewRefresh(columns, ddbbConfig, alteredFilterState);
+    };
+
+  const onChangeOperatorHandler =
+    (operatorIndex: number) =>
+    (event: SelectChangeEvent<string>, child: React.ReactNode) => {
+      const alteredFilterState = { ...filters };
+      alteredFilterState.conditions[operatorIndex].operator =
+        event.target.value;
+      configActions.alterFilters(alteredFilterState);
+      dataActions.dataviewRefresh(columns, ddbbConfig, alteredFilterState);
+    };
+
   const existedColumnSelector = (selectorProps: {
     currentCol: string;
     index: number;
-  }) => (
-    <FormControl
-      fullWidth
-      key={`FormControl-existedColumnSelector-${selectorProps.index}`}
-    >
-      <Select
-        value={selectorProps.currentCol}
-        size="small"
-        key={`Select-existedColumnSelector-${selectorProps.index}`}
-        //onChange={handleChange}
+  }) => {
+    return (
+      <FormControl
+        fullWidth
+        key={`FormControl-existedColumnSelector-${selectorProps.index}`}
       >
-        {possibleColumns.map((key) => {
-          return (
-            <MenuItem
-              value={key}
-              key={`MenuItem-existedColumnSelector-${key}-${selectorProps.index}`}
-            >
-              {key}
-            </MenuItem>
-          );
-        })}
-      </Select>
-    </FormControl>
-  );
+        <Select
+          value={selectorProps.currentCol}
+          size="small"
+          key={`Select-existedColumnSelector-${selectorProps.index}`}
+          onChange={onchangeExistedColumnHandler(selectorProps.index)}
+        >
+          {possibleColumns.map((key) => {
+            return (
+              <MenuItem
+                value={key}
+                key={`MenuItem-existedColumnSelector-${key}-${selectorProps.index}`}
+              >
+                {key}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    );
+  };
 
   const operatorSelector = (selectorProps: {
     currentOp: string;
     index: number;
-  }) => (
-    <FormControl fullWidth>
-      <Select
-        value={selectorProps.currentOp}
-        size="small"
-        //onChange={handleChange}
-      >
-        {Object.entries(OperatorFilter).map(([key, value]) => {
-          return (
-            <MenuItem
-              value={key}
-              key={`MenuItem-OperatorSelector-${key}-${selectorProps.index}`}
-            >
-              {value}
-            </MenuItem>
-          );
-        })}
-      </Select>
-    </FormControl>
-  );
+  }) => {
+    return (
+      <FormControl fullWidth>
+        <Select
+          value={selectorProps.currentOp}
+          size="small"
+          onChange={onChangeOperatorHandler(selectorProps.index)}
+        >
+          {Object.entries(OperatorFilter).map(([key, value]) => {
+            return (
+              <MenuItem
+                value={key}
+                key={`MenuItem-OperatorSelector-${key}-${selectorProps.index}`}
+              >
+                {value}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    );
+  };
 
   const currentFilters = () => {
     return (
@@ -116,7 +148,7 @@ const DataviewFiltersPortal = (props: DataviewFiltersProps) => {
               ...styles.popper,
               zIndex: 4,
               minWidth: 200,
-              maxWidth: 320,
+              maxWidth: 420,
               padding: "0.75rem",
               background: StyleVariables.BACKGROUND_SECONDARY,
             }}
@@ -137,7 +169,7 @@ const DataviewFiltersPortal = (props: DataviewFiltersProps) => {
                         index: index,
                       })}
                     </Grid>
-                    <Grid item xs={3} key={`Grid-operator-${index}`}>
+                    <Grid item xs={2} key={`Grid-operator-${index}`}>
                       {operatorSelector({ currentOp: operator, index: index })}
                     </Grid>
                     {/* if value exists, show it */}
@@ -151,6 +183,16 @@ const DataviewFiltersPortal = (props: DataviewFiltersProps) => {
                         />
                       </Grid>
                     )}
+                    {/* Remove button */}
+                    <Grid item xs={0.25} key={`Grid-remove-${index}`}>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        size="small"
+                      >
+                        <CrossIcon />
+                      </Button>
+                    </Grid>
                   </Grid>
                 );
               })}
