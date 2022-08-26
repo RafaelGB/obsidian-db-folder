865b56d50a9e2d1aac117f1c6c014e26ea9849f4
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-08-26 08:53:07 +0200

    pagination works with cool menu

---
+++ src/components/PaginationTable.tsx
@@ -0,0 +1,119 @@
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import TextField from "@mui/material/TextField";
+import { PaginationProps } from "cdm/MenuBarModel";
+import React from "react";
+function PaginationTable(props: PaginationProps) {
+  const { table } = props;
+  const [page, setPage] = React.useState(null);
+  return (
+    <>
+      <ButtonGroup
+        variant="text"
+        size="small"
+        key={`ButtonGroup-PaginationTable`}
+      >
+        <Button
+          size="small"
+          key={`Button-Pagination-Initial`}
+          onClick={() => {
+            table.setPageIndex(0);
+            setPage("");
+          }}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<<"}
+        </Button>
+        <Button
+          size="small"
+          key={`Button-Pagination-Previous`}
+          onClick={() => {
+            table.previousPage();
+            setPage(table.getState().pagination.pageIndex);
+          }}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </Button>
+        <TextField
+          id="Input-Pagination-number-id"
+          size="small"
+          key={`Input-Pagination-number`}
+          label={`Page of ${table.getPageCount()}`}
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          style={{ width: "100px" }}
+          InputProps={{
+            inputProps: {
+              style: { textAlign: "center" },
+            },
+          }}
+          value={page}
+          onChange={(e) => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0;
+            table.setPageIndex(page);
+            setPage(e.target.value ? Number(e.target.value) : null);
+          }}
+        />
+        <Button
+          size="small"
+          key={`Button-Pagination-Next`}
+          onClick={() => {
+            table.nextPage();
+            setPage(table.getState().pagination.pageIndex + 2);
+          }}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </Button>
+        <Button
+          size="small"
+          key={`Button-Pagination-Last`}
+          onClick={() => {
+            table.setPageIndex(table.getPageCount() - 1);
+            setPage(table.getPageCount());
+          }}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </Button>
+        {/* <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select> */}
+      </ButtonGroup>
+      {/* <div>{table.getRowModel().rows.length} Rows</div> */}
+    </>
+  );
+}
+
+export default PaginationTable;
