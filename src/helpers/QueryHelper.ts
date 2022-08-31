372b998fe0b7eedeaf2abf23917bcf8afc6b7f02
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-08-31 18:38:40 +0200

    supports inline with bold, italic and not starting line

--- src/helpers/QueryHelper.ts
+++ src/helpers/QueryHelper.ts
@@ -1,9 +1,21 @@
 import { TableColumn } from "cdm/FolderModel";
 import { DatabaseCore } from "helpers/Constants";
+import { LOGGER } from "services/Logger";
 
 export function generateDataviewTableQuery(columns: TableColumn[], fromQuery: string): string {
     return `TABLE ${columns
         .filter((col) => !col.isMetadata)
         .map((col) => col.key)
         .join(",")},${DatabaseCore.DATAVIEW_FILE},${DatabaseCore.FRONTMATTER_KEY} ${fromQuery}`
+}
+
+
+export function inlineRegexInFunctionOf(columnId: string) {
+    const wrappererKey = `_\\*~\``;
+    const conditionalInitWithField = `[${wrappererKey}]{0,2}${columnId}[${wrappererKey}]{0,2}[:]{2}`;
+    const thenRegex = `(^${conditionalInitWithField})(.*$)`;
+    const elseRegex = `(.*)([(])(${conditionalInitWithField})(.*)([)])(.*$)`;
+    const finalExpression = `(?:(?=(^${conditionalInitWithField})(${thenRegex})|(${elseRegex}))`;
+    LOGGER.debug(`<=> inlineRegexInFunctionOf: ${finalExpression}`);
+    return new RegExp(`(?:(?=(^${conditionalInitWithField}))(${thenRegex})|(${elseRegex}))`, 'gm');
 }
