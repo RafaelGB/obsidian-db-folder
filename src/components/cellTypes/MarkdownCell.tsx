7aba13b56733878764cfc0d8da4727b7a7ce5f88
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-09-08 10:41:16 +0200

    markdown ordered

--- src/components/cellTypes/MarkdownCell.tsx
+++ src/components/cellTypes/MarkdownCell.tsx
@@ -1,17 +1,24 @@
 import { CellComponentProps } from "cdm/ComponentsModel";
 import { renderMarkdown } from "components/obsidianArq/MarkdownRenderer";
 import { c } from "helpers/StylesHelper";
+import { Link } from "obsidian-dataview";
 import React, { useEffect, useRef } from "react";
 
 const MarkdownCell = (mdProps: CellComponentProps) => {
   const { defaultCell } = mdProps;
   const { cell } = defaultCell;
-  const cellValue = cell.getValue();
+  const cellValue = cell.getValue() as string;
   const mdRef = useRef<HTMLDivElement>();
   useEffect(() => {
     if (mdRef.current !== null) {
       mdRef.current.innerHTML = "";
-      renderMarkdown(defaultCell, cellValue?.toString(), mdRef.current, 5);
+      const split = cellValue.split("|");
+      renderMarkdown(
+        defaultCell,
+        `[[${split[1]}|${split[0]}]]`,
+        mdRef.current,
+        5
+      );
     }
   });
   return <span ref={mdRef} className={`${c("md_cell")}`}></span>;
