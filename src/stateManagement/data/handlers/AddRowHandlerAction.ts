5d7b9b2b008729d435eb45aa239a2dc04b817a69
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-09-12 12:31:54 +0200

    hotfix of addrow and import

--- src/stateManagement/data/handlers/AddRowHandlerAction.ts
+++ src/stateManagement/data/handlers/AddRowHandlerAction.ts
@@ -4,7 +4,7 @@ import { LocalSettings } from "cdm/SettingsModel";
 import { DataState, TableActionResponse } from "cdm/TableStateInterface";
 import { DatabaseView } from "DatabaseView";
 import { SourceDataTypes } from "helpers/Constants";
-import { resolve_tfolder } from "helpers/FileManagement";
+import { resolve_tfile, resolve_tfolder } from "helpers/FileManagement";
 import { DateTime } from "luxon";
 import { Link } from "obsidian-dataview";
 import { VaultManagerDB } from "services/FileManagerService";
@@ -13,14 +13,14 @@ import { AbstractTableAction } from "stateManagement/AbstractTableAction";
 
 export default class AddRowlHandlerAction extends AbstractTableAction<DataState> {
     handle(tableActionResponse: TableActionResponse<DataState>): TableActionResponse<DataState> {
-        const { view, set, implementation } = tableActionResponse;
-        implementation.actions.addRow = (filename: string, columns: TableColumn[], ddbbConfig: LocalSettings) => set((state) => {
+        const { view, set, get, implementation } = tableActionResponse;
+        implementation.actions.addRow = async (filename: string, columns: TableColumn[], ddbbConfig: LocalSettings) => {
             const destination_folder = this.destination_folder(view, ddbbConfig);
             let trimedFilename = filename.replace(/\.[^/.]+$/, "").trim();
             let filepath = `${destination_folder}/${trimedFilename}.md`;
             // Validate possible duplicates
             let sufixOfDuplicate = 0;
-            while (state.rows.find((row) => row.__note__.filepath === filepath)) {
+            while (get().rows.find((row) => row.__note__.filepath === filepath)) {
                 sufixOfDuplicate++;
                 filepath = `${destination_folder}/${trimedFilename}-${sufixOfDuplicate}.md`;
             }
@@ -28,6 +28,7 @@ export default class AddRowlHandlerAction extends AbstractTableAction<DataState>
                 trimedFilename = `${trimedFilename}-${sufixOfDuplicate}`;
                 filename = `${trimedFilename} copy(${sufixOfDuplicate})`;
             }
+
             const rowRecord: RowDatabaseFields = { inline: {}, frontmatter: {} };
             columns
                 .filter((column: TableColumn) => !column.isMetadata)
@@ -39,12 +40,13 @@ export default class AddRowlHandlerAction extends AbstractTableAction<DataState>
                     }
                 });
             // Add note to persist row
-            VaultManagerDB.create_markdown_file(
+            await VaultManagerDB.create_markdown_file(
                 resolve_tfolder(destination_folder),
                 trimedFilename,
                 rowRecord,
                 ddbbConfig
             );
+
             const newNote = new NoteInfo({
                 ...rowRecord.frontmatter,
                 ...rowRecord.inline,
@@ -52,7 +54,22 @@ export default class AddRowlHandlerAction extends AbstractTableAction<DataState>
                     path: filepath,
                     ctime: DateTime.now(),
                     mtime: DateTime.now(),
-                    link: Link.file(filepath),
+                    link: {
+                        path: filepath,
+                        fileName: () => filename,
+                        type: "file",
+                        embed: false,
+                        equals: (link: Link) => link.path === filepath,
+                        toObject: () => ({ path: filepath }),
+                        withPath: null,
+                        withDisplay: null,
+                        withHeader: null,
+                        toEmbed: null,
+                        toFile: null,
+                        markdown: () => `[[${filepath}|${filename}]]`,
+                        fromEmbed: null,
+                        obsidianLink: () => `[[${filepath}|${filename}]]`,
+                    },
                     tasks: [],
                     inlinks: [],
                     outlinks: [],
@@ -60,8 +77,11 @@ export default class AddRowlHandlerAction extends AbstractTableAction<DataState>
             });
 
             const row: RowDataType = newNote.getRowDataType(columns, ddbbConfig);
-            return { rows: [...state.rows, row] }
-        });
+            set((state) => {
+
+                return { rows: [...state.rows, row] }
+            })
+        };
         tableActionResponse.implementation = implementation;
         return this.goNext(tableActionResponse);
     }
