b2413953fb56115c49dc0b4fa8514b07da2ab489
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-09-27 12:29:08 +0200

    stringnify db yaml

--- src/parsers/RowDatabaseFieldsToFile.ts
+++ src/parsers/RowDatabaseFieldsToFile.ts
@@ -1,18 +1,22 @@
 import { RowDatabaseFields } from "cdm/DatabaseModel";
 import { LocalSettings } from "cdm/SettingsModel";
 import { InputType } from "helpers/Constants";
-import { stringifyYaml } from "obsidian";
+import { Literal } from "obsidian-dataview";
+import { DateTime } from "luxon";
 import { DataviewService } from "services/DataviewService";
-export const parseFrontmatterFieldsToString = (databaseFields: RowDatabaseFields, deletedColumn?: string): string => {
+export const parseFrontmatterFieldsToString = (databaseFields: RowDatabaseFields, localSettings: LocalSettings, deletedColumn?: string): string => {
     const frontmatterFields = databaseFields.frontmatter;
-    delete frontmatterFields[deletedColumn];
-    let stringify: string = "";
-    if (Object.keys(frontmatterFields).length > 0) {
-        const stringifiedFrontmatter = stringifyYaml(frontmatterFields);
-        stringify = `---\n${stringifiedFrontmatter}---`;
+    let array: string[] = [];
+    Object.keys(frontmatterFields).forEach(key => {
+        if (key !== deletedColumn) {
+            array.push(...stringifyDbYaml(frontmatterFields[key], 0, localSettings, key));
+        }
+    });
+    if (array.length > 0) {
+        array = [`---`, ...array, `---`];
     }
-    return stringify;
-}
+    return array.join('\n');
+};
 
 export const parseInlineFieldsToString = (inlineFields: RowDatabaseFields): string => {
     const array: string[] = [];
@@ -24,4 +28,36 @@ export const parseInlineFieldsToString = (inlineFields: RowDatabaseFields): stri
 
 export function parseValuetoSanitizeYamlValue(value: string, localSettings: LocalSettings): string {
     return DataviewService.parseLiteral(value, InputType.MARKDOWN, localSettings).toString();
+}
+
+function stringifyDbYaml(literal: Literal, level: number, localSettings: LocalSettings, key?: string): string[] {
+    const literalBlock: string[] = [];
+    literal = DataviewService.parseDataArray(literal);
+    // Manage Arrays
+    if (DataviewService.getDataviewAPI().value.isArray(literal)) {
+        literalBlock.push(`${" ".repeat(level)}${key}:`);
+        literal.forEach((literal, index) => {
+            literalBlock.push(...stringifyDbYaml(literal, level + 1, localSettings));
+        });
+    }
+    // Manage Dates
+    else if (DateTime.isDateTime(literal)) {
+        literalBlock.push(`${" ".repeat(level)}${key}: ${DataviewService.parseLiteral(literal, InputType.MARKDOWN, localSettings)}`);
+    }
+    // Manage Objects
+    else if (DataviewService.getDataviewAPI().value.isObject(literal)) {
+
+        literalBlock.push(`${" ".repeat(level)}${key}:`);
+        Object.entries(literal).forEach(([key, value]) => {
+            literalBlock.push(...stringifyDbYaml(value, level + 1, localSettings, key));
+        }
+        );
+    }
+    // Manage atomic values
+    else if (key) {
+        literalBlock.push(`${" ".repeat(level)}${key}: ${DataviewService.parseLiteral(literal, InputType.MARKDOWN, localSettings)}`);
+    } else {
+        literalBlock.push(`${" ".repeat(level)}- ${DataviewService.parseLiteral(literal, InputType.MARKDOWN, localSettings)}`);
+    }
+    return literalBlock;
 }
