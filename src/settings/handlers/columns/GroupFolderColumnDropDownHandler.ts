ad2da2e5272a4ea5104ea09ef0b9ef2b55a92b1c
Y <myacine70@gmail.com>
2022-09-28 07:55:53 +0000

    feat: gracefully handle wrong casing when setting group_folder_column

--- src/settings/handlers/columns/GroupFolderColumnDropDownHandler.ts
+++ src/settings/handlers/columns/GroupFolderColumnDropDownHandler.ts
@@ -1,6 +1,23 @@
 import { InputType } from "helpers/Constants";
+import { Notice } from "obsidian";
 import { AbstractSettingsHandler, SettingHandlerResponse } from "settings/handlers/AbstractSettingHandler";
 import { add_text } from "settings/SettingsComponents";
+
+const createNoticeDebouncer = () =>{
+    const timeout: { current: ReturnType<typeof setTimeout>; } = { current: null };
+    return {
+      notice: (
+        message: string,
+        messageDelay: number,
+        debounceDelay: number,
+      ) => {
+        clearTimeout(timeout.current);
+        timeout.current = setTimeout(() => { new Notice(message, messageDelay); }, debounceDelay);
+      },
+      cleanup: () => clearTimeout(timeout.current),
+    };
+} 
+
 
 export class GroupFolderColumnDropDownHandler extends AbstractSettingsHandler {
   settingTitle: string = 'Choose columns to organize files into subfolders'
@@ -13,25 +30,27 @@ export class GroupFolderColumnDropDownHandler extends AbstractSettingsHandler {
                 .filter( (f) => columns[f].input === InputType.SELECT)
                 .map((key) => columns[key].label),
             );
-
+            const lowerCaseAllowedColumns = new Set( Array.from(allowedColumns).map((f) => f.toLowerCase()),);
+            const lowerCaseAllowedColumnsMap = new Map( Array.from(allowedColumns).map((key) => [key.toLowerCase(), key]),)
+            const debouncedNotice = createNoticeDebouncer();
             const group_folder_column_input_promise =
-                async (value: string): Promise<void> => {
-                if (
-                    value
-                    .split(",")
-                    .every((column) =>
-                        allowedColumns.has(column),
-                    )
-                )
-                    view.diskConfig.updateConfig({
-                    group_folder_column: value,
-                    });
-                else
-                    throw new Error(
-                    value +
-                        " is an invalid value for group_folder_column",
-                    );
-                };
+              async ( value: string ): Promise<void> => {
+                const validConfig = value
+                  .split(",")
+                  .every((column) => lowerCaseAllowedColumns.has(column.toLowerCase()));
+                if (validConfig){
+                    debouncedNotice.cleanup();
+                    // make sure the case of each column is correct
+                    const correctCaseColumns = value
+                        .split(",")
+                        .map((column) => lowerCaseAllowedColumnsMap.get(column.toLowerCase()))
+                        .join(",");
+                    view.diskConfig.updateConfig({ group_folder_column: correctCaseColumns });
+                }
+                else {
+                    debouncedNotice.notice(`[${value}] is an invalid value for group_folder_column`, 1500, 1500)
+                }
+              };
 
             add_text(
                 containerEl,
