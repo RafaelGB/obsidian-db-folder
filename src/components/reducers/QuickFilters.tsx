98425b57f0d1f5d1b44f3a6d5e61ff92753bbe0f
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-09-30 20:38:12 +0200

    skeleton working with chips

--- src/components/reducers/QuickFilters.tsx
+++ src/components/reducers/QuickFilters.tsx
@@ -1,26 +1,45 @@
 import Chip from "@mui/material/Chip";
-import Input from "@mui/material/Input";
 import Stack from "@mui/material/Stack";
 import { DataviewFiltersProps } from "cdm/ComponentsModel";
-import { FilterGroup, FilterGroupCondition } from "cdm/SettingsModel";
-import React, { useMemo, useState } from "react";
+import { FilterGroupCondition } from "cdm/SettingsModel";
+import React from "react";
 
 const QuickFilters = (props: DataviewFiltersProps) => {
   const { table } = props;
-  const { tableState, view } = table.options.meta;
+  const { tableState } = table.options.meta;
+  const configActions = tableState.configState((state) => state.actions);
+  const dataActions = tableState.data((state) => state.actions);
   const configInfo = tableState.configState((state) => state.info);
+  const columnsInfo = tableState.columns((state) => state.info);
   const filters = tableState.configState((state) => state.filters);
-  const quickConditions: FilterGroup[] = useMemo(
-    () =>
-      filters.conditions.filter(
-        (condition) => (condition as FilterGroupCondition).condition
-      ),
-    [filters.conditions]
-  );
+  const toggleFilterGroupOnClickHandler = (filterIndex: number) => () => {
+    const alteredFilterState = { ...configInfo.getFilters() };
+    const currentValue = filters.conditions[
+      filterIndex
+    ] as FilterGroupCondition;
+
+    (
+      alteredFilterState.conditions[filterIndex] as FilterGroupCondition
+    ).disabled = !currentValue.disabled;
+
+    configActions.alterFilters(filters);
+    dataActions.dataviewRefresh(
+      columnsInfo.getAllColumns(),
+      configInfo.getLocalSettings(),
+      alteredFilterState
+    );
+  };
+
   return (
     <Stack direction="row" spacing={1}>
-      {quickConditions.map((condition, index) => {
-        return <Chip size="small" label={`filter-${index}`} />;
+      {filters.conditions.map((condition, filterIndex) => {
+        if ((condition as FilterGroupCondition).condition) {
+          return (
+            <div onClick={toggleFilterGroupOnClickHandler(filterIndex)}>
+              <Chip size="small" label={`filter-${filterIndex}`} />
+            </div>
+          );
+        }
       })}
     </Stack>
   );
