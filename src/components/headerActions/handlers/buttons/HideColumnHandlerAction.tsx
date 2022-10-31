1a933d50068e55b689cea7aeec3fa93afd668a9f
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-10-31 20:01:07 +0100

    code smell of typos

--- src/components/headerActions/handlers/buttons/HideColumnHandlerAction.tsx
+++ src/components/headerActions/handlers/buttons/HideColumnHandlerAction.tsx
@@ -29,7 +29,7 @@ function hideButton(headerActionResponse: HeaderActionResponse) {
     (store) => store.actions
   );
 
-  const onClick = () => {
+  const onClick = async () => {
     const currentCol = column.columnDef as TableColumn;
     column.getToggleVisibilityHandler()({ target: { checked: false } });
     columnActions.alterIsHidden(currentCol, true);
