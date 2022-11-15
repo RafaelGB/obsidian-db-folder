6feb204468ed92528e52827ab618ed237548dc79
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-11-15 09:17:27 +0100

    improving hover link

--- src/components/RowContextMenu.tsx
+++ src/components/RowContextMenu.tsx
@@ -5,7 +5,7 @@ import {
   MetadataDatabaseColumns,
   StyleVariables,
 } from "helpers/Constants";
-import React from "react";
+import React, { useState } from "react";
 import { showFileMenu } from "components/obsidianArq/commands";
 import Relationship from "components/RelationShip";
 import LinkIcon from "@mui/icons-material/Link";
@@ -20,7 +20,7 @@ const rowContextMenuColumn: TableColumn = {
   maxSize: 30,
   cell: ({ row, table }) => {
     const { tableState } = table.options.meta;
-    const [isHovering, setIsHovering] = React.useState(false);
+    const [isHovering, setIsHovering] = useState(false);
     const rowActions = tableState.data((state) => state.actions);
     const handleDeleteRow = () => {
       rowActions.removeRow(row.original);
@@ -30,35 +30,54 @@ const rowContextMenuColumn: TableColumn = {
       rowActions.renameFile(row.index);
     };
 
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-      // Handle right click
-      if (event.type === "contextmenu") {
-        showFileMenu(
-          row.original.__note__.getFile(),
-          event.nativeEvent,
-          handleDeleteRow,
-          handleRenameRow
-        );
-      }
-      // Handle left click
-      else if (event.type === "click") {
-        app.workspace.getLeaf().openFile(row.original.__note__.getFile());
+    /**
+     * Handle left click
+     * @param event
+     */
+    const handleClick = async () => {
+      await app.workspace.getLeaf().openFile(row.original.__note__.getFile());
+    };
+
+    /**
+     * Handle right click
+     * @param event
+     */
+    const handlerContextMenu = async (event: React.MouseEvent<HTMLElement>) => {
+      showFileMenu(
+        row.original.__note__.getFile(),
+        event.nativeEvent,
+        handleDeleteRow,
+        handleRenameRow
+      );
+    };
+
+    const index = Number(row.index) + 1;
+
+    const handleMouseOver = () => {
+      if (!isHovering) {
+        setIsHovering(true);
       }
     };
-    const index = Number(row.index) + 1;
+
+    const handleMouseOut = () => {
+      if (isHovering) {
+        setTimeout(() => setIsHovering(false), 80);
+      }
+    };
     return (
       <>
         <div
           onClick={handleClick}
-          onContextMenu={handleClick}
-          onMouseOver={() => setIsHovering(true)}
-          onMouseOut={() => setTimeout(() => setIsHovering(false), 150)}
+          onContextMenu={handlerContextMenu}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          onMouseLeave={handleMouseOut}
           key={`row-context-button-${index}`}
           style={{
             alignItems: "center",
             display: "flex",
             justifyContent: "center",
-            height: "5px",
+            height: "30px",
             width: "20px",
           }}
         >
