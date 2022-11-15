a7c39341bc3d2c8f564f99507dcef7536f01a147
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-11-15 13:59:20 +0100

    cell size default options

--- src/settings/handlers/folder/DetailsFormHandler.ts
+++ src/settings/handlers/folder/DetailsFormHandler.ts
@@ -4,31 +4,32 @@ import { add_text } from "settings/SettingsComponents";
 export class DetailsFormHandler extends AbstractSettingsHandler {
     settingTitle: string = 'Information about your database:';
     handle(settingHandlerResponse: SettingHandlerResponse): SettingHandlerResponse {
-        const details_edit_name_promise = async (value: string): Promise<void> => {
-            view.diskConfig.updateYaml('name', value);
-        }
+        const { containerEl, view, local } = settingHandlerResponse;
+        if (local) {
+            const details_edit_name_promise = async (value: string): Promise<void> => {
+                view.diskConfig.updateYaml('name', value);
+            }
 
-        const details_edit_desciption_promise = async (value: string): Promise<void> => {
-            view.diskConfig.updateYaml('description', value);
+            const details_edit_desciption_promise = async (value: string): Promise<void> => {
+                view.diskConfig.updateYaml('description', value);
+            }
+            add_text(
+                containerEl,
+                this.settingTitle.concat(' (name)'),
+                "name of the database",
+                "type title...",
+                view.diskConfig.yaml.name,
+                details_edit_name_promise
+            );
+            add_text(
+                containerEl,
+                this.settingTitle.concat(' (description)'),
+                "description of the database",
+                "type description...",
+                view.diskConfig.yaml.description,
+                details_edit_desciption_promise
+            );
         }
-        const { containerEl, view } = settingHandlerResponse;
-
-        add_text(
-            containerEl,
-            this.settingTitle.concat(' (name)'),
-            "name of the database",
-            "type title...",
-            view.diskConfig.yaml.name,
-            details_edit_name_promise
-        );
-        add_text(
-            containerEl,
-            this.settingTitle.concat(' (description)'),
-            "description of the database",
-            "type description...",
-            view.diskConfig.yaml.description,
-            details_edit_desciption_promise
-        );
         return this.goNext(settingHandlerResponse);
     }
 }
