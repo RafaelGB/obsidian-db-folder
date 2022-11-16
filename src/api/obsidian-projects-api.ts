9d20c598413aa2bbcbb1ac57ac1907e73bc6e4b8
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-11-16 16:28:13 +0100

    support for types

--- src/api/obsidian-projects-api.ts
+++ src/api/obsidian-projects-api.ts
@@ -1,17 +1,20 @@
-import { DB_ICONS, SourceDataTypes } from "helpers/Constants";
+import { DatabaseCore, DB_ICONS, DEFAULT_COLUMN_CONFIG, InputType, SourceDataTypes } from "helpers/Constants";
 import DBFolderPlugin from "main";
 
 import {
     DatabaseView,
 } from 'DatabaseView';
 import { LOGGER } from "services/Logger";
-import { DataQueryResult, ProjectView, ProjectViewProps } from "obsidian-projects-types";
+import { DataFieldType, DataQueryResult, ProjectView, ProjectViewProps } from "obsidian-projects-types";
 import { resolve_tfile, resolve_tfolder } from "helpers/FileManagement";
 import { generateDbConfiguration, generateNewDatabase } from "helpers/CommandsHelper";
 import { LocalSettings } from "cdm/SettingsModel";
+import { DatabaseColumn } from "cdm/DatabaseModel";
+import { dbTrim } from "helpers/StylesHelper";
 
 class ProjectAPI extends ProjectView {
     private plugin: DBFolderPlugin;
+    private view: DatabaseView;
 
     constructor(plugin: DBFolderPlugin) {
         super();
@@ -33,15 +36,42 @@ class ProjectAPI extends ProjectView {
 
     async onData({ data }: DataQueryResult) {
         const { fields } = data;
-        fields.forEach((field) => {
-            const { name, type, identifier, derived } = field;
-            /**
-             * I can not use the view object here without make it a class variable
-             * and I don't want to do that because I can not support multiple views
-             * 
-             * Could we add the config to the data object? I can manage a map of views with that
-             */
-        });
+        const currentColumnsLength = Object
+            .values(this.view.diskConfig.yaml.columns)
+            .filter((column) => !column.isMetadata).length;
+        const actualFields = fields
+            .filter((field) => ![DatabaseCore.FRONTMATTER_KEY, "File"].contains(field.name));
+        if (currentColumnsLength !== actualFields.length) {
+            const newColumns: Record<string, DatabaseColumn> = {};
+            fields.forEach((field, index) => {
+                const { name, type } = field;
+                /**
+                 * I can not use the view object here without make it a class variable
+                 * and I don't want to do that because I can not support multiple views
+                 * 
+                 * Could we add the config to the data object? I can manage a map of views with that
+                 */
+                const inputType = this.mapperTypeToInputType(type);
+                const key = dbTrim(name);
+                const newColumn: DatabaseColumn = {
+                    input: inputType,
+                    accessorKey: key,
+                    key: key,
+                    id: key,
+                    label: name,
+                    position: index,
+                    config: {
+                        ...DEFAULT_COLUMN_CONFIG,
+                        isInline: false
+                    },
+                };
+                newColumns[name] = newColumn;
+
+            });
+
+            this.view.diskConfig.yaml.columns = newColumns;
+            await this.view.diskConfig.saveOnDisk();
+        }
     }
 
     // onOpens is called whenever the user activates your view.
@@ -63,11 +93,11 @@ class ProjectAPI extends ProjectView {
         }
         const leaf = app.workspace.getLeaf();
         const file = resolve_tfile(filePath);
-        const view = new DatabaseView(leaf, this.plugin, file);
-        view.initRootContainer(file);
-        await view.initDatabase();
+        this.view = new DatabaseView(leaf, this.plugin, file);
+        this.view.initRootContainer(file);
+        await this.view.initDatabase();
         LOGGER.debug("Database initialized successfully from project view");
-        this.dataEl = contentEl.createDiv().appendChild(view.containerEl);
+        this.dataEl = contentEl.createDiv().appendChild(this.view.containerEl);
     }
 
     async onClose() {
@@ -80,25 +110,53 @@ class ProjectAPI extends ProjectView {
             ...this.plugin.settings.local_settings,
         }
         if (project.dataview) {
+            localSettings.source_destination_path = project.path;
             localSettings.source_data = SourceDataTypes.QUERY;
             /* 
             * Check if the query contains FROM or from
             * then Split query to only get subtring from "from" to the end
             */
             let query = "";
-            if (project.query?.contains("FROM")) {
-                query = project.query?.split("FROM")[1];
-            } else if (project.query?.contains("from")) {
-                query = project.query?.split("from")[1];
+            const SOURCE_FLAG = "FROM";
+            if (project.query?.contains(SOURCE_FLAG)) {
+                query = project.query?.split(SOURCE_FLAG)[1];
+            } else if (project.query?.contains(SOURCE_FLAG.toLowerCase())) {
+                query = project.query?.split(SOURCE_FLAG.toLowerCase())[1];
             } else {
                 // Handle error with default configuation
                 localSettings.source_data = SourceDataTypes.CURRENT_FOLDER;
-                LOGGER.error("The query does not contain a FROM clause. Using current folder as source data");
+                LOGGER.error(`The query does not contain a ${SOURCE_FLAG} clause. Using current folder as source data`);
             }
-            localSettings.source_destination_path = query
+            localSettings.source_form_result = `${SOURCE_FLAG} ${query}`;
         }
 
         return localSettings;
+    }
+
+    private mapperTypeToInputType(type: string): string {
+        let inputType = "";
+        switch (type) {
+            case DataFieldType.String:
+            case DataFieldType.Link:
+            case DataFieldType.Unknown:
+                inputType = InputType.TEXT;
+                break;
+            case DataFieldType.Number:
+                inputType = InputType.NUMBER;
+                break;
+            case DataFieldType.Boolean:
+                inputType = InputType.CHECKBOX;
+                break;
+            case DataFieldType.Date:
+                inputType = InputType.CALENDAR;
+                break;
+            case DataFieldType.List:
+                inputType = InputType.TAGS;
+                break;
+            default:
+                inputType = InputType.TEXT;
+        }
+        return inputType;
     }
 }
 
