28fa28936ac6f7d3f51ba9a238529163ef96cc85
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-11-30 13:43:42 +0100

    boundary

--- src/components/ErrorComponents.tsx
+++ src/components/ErrorComponents.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { Component, ErrorInfo, ReactNode } from "react";
+import { LOGGER } from "services/Logger";
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
 
 export function boundaryPreRendererComponent(errors: Record<string, string[]>) {
   return (
     <div className="p-dbfolder-error">
-      <h2>Errors</h2>
+      <h2>An error has occurred before rendering the table</h2>
       {/* List all recorded errors*/}
       <ul>
         {Object.keys(errors).map((key, indexGroup) => {
@@ -25,3 +33,30 @@ export function boundaryPreRendererComponent(errors: Record<string, string[]>) {
     </div>
   );
 }
+/**
+ * Error boundary component to catch errors in the table
+ */
+class DbErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    LOGGER.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h1>Sorry.. there was an error</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default DbErrorBoundary;
