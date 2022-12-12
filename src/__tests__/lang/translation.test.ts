8bcfb41cb78e80ea3d1299f0ff7959f7a2bf047c
Rafael Gómez Bermejo <sernn2@gmail.com>
2022-12-12 21:09:15 +0100

    code smells and some testing

---
+++ src/__tests__/lang/translation.test.ts
@@ -0,0 +1,12 @@
+import { t } from "lang/helpers";
+import { mockLocalStorage } from "mock/mockWindowUtils";
+describe("LANG.t", () => {
+    /** Metadata columns */
+    test('t function call', () => {
+        const { setItemMock } = mockLocalStorage();
+        setItemMock('language', 'en');
+        const testTranslationValue = t("confirm_modal_ok")
+        expect(testTranslationValue).toBe('Yes');
+
+    });
+}); 
