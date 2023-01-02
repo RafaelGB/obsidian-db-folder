8268ce9b4dc4b9209e8836eab573a4d88ee957ba
Rafael Gómez Bermejo <sernn2@gmail.com>
2023-01-02 13:06:52 +0100

    fn docu

--- src/cdm/ModulesFnModel.ts
+++ src/cdm/ModulesFnModel.ts
@@ -2,19 +2,53 @@ import { Literal } from "obsidian-dataview"
 import { DateTime, DurationLikeObject } from "luxon";
 
 type NumbersFnType = {
+    /**
+     * Summatory of the list of values
+     * @param values 
+     * @returns 
+     */
     sum: (values: Literal[]) => number;
+    /**
+     * Obtains the minimum value of the list of values
+     * @param values
+     * @returns
+     */
     min: (values: Literal[]) => number;
+    /**
+     * Obtains the maximum value of the list of values
+     * @param values
+     * @returns
+     */
     max: (values: Literal[]) => number;
     [key: string]: unknown
 }
 
 type LuxonFnType = {
+    /**
+     * Returns the earliest date in the list of values
+     * @param values 
+     * @returns 
+     */
     earliest: (values: Literal[]) => DateTime;
+    /**
+     * Returns the latest date in the list of values
+     * @param values
+     * @returns
+     */
     latest: (values: Literal[]) => DateTime;
+    /**
+     * Returns the duration between the earliest and latest dates in the list of values
+     * @param values
+     * @param dl - Duration like object key. "days" by default. See https://www.jsdocs.io/package/@types/luxon#DurationLikeObject
+     * @returns
+     */
     range: (values: Literal[], dl?: keyof DurationLikeObject) => number;
     [key: string]: unknown
 }
 
+/**
+ * Exposed functionalities under the `db` variable of Formulas
+ */
 export type DatabaseFnType = {
     numbers: NumbersFnType;
     luxon: LuxonFnType;
