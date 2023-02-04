03ee355a67d77d5f7a15bea87a1a51533fae7f2b
Rafael Gómez Bermejo <sernn2@gmail.com>
2023-02-04 20:06:02 +0100

    new css for large descriptions

--- src/components/modals/columnSettings/handlers/selects/SelectedColumnOptionsHandler.tsx
+++ src/components/modals/columnSettings/handlers/selects/SelectedColumnOptionsHandler.tsx
@@ -1,100 +1,18 @@
 import { RowSelectOption } from "cdm/ComponentsModel";
 import { ColumnSettingsHandlerResponse } from "cdm/ModalsModel";
-import { randomColor, castStringtoHsl, castHslToString } from "helpers/Colors";
-import { satinizedColumnOption } from "helpers/FileManagement";
+import { castStringtoHsl, castHslToString } from "helpers/Colors";
 import { t } from "lang/helpers";
-import { ButtonComponent, Notice, Setting } from "obsidian";
+import { Notice, Setting } from "obsidian";
 import { AbstractHandlerClass } from "patterns/chain/AbstractHandler";
 import { LOGGER } from "services/Logger";
-import { add_setting_header } from "settings/SettingsComponents";
 
 export class SelectedColumnOptionsHandler extends AbstractHandlerClass<ColumnSettingsHandlerResponse> {
   settingTitle = t("column_settings_modal_selected_column_options_title");
   handle(
     columnHandlerResponse: ColumnSettingsHandlerResponse
   ): ColumnSettingsHandlerResponse {
-    const { column, containerEl, columnSettingsManager } =
-      columnHandlerResponse;
-    const { view } = columnSettingsManager.modal;
-    let newLabel = "";
+    const { column, containerEl } = columnHandlerResponse;
     const options = column.options;
-    const addLabelPromise = async (): Promise<void> => {
-      // Error handling
-      if (newLabel === "") {
-        new Notice(
-          t(
-            "column_settings_modal_selected_column_options_notice_error_empty_label"
-          ),
-          1500
-        );
-        return;
-      }
-      if (options.find((option) => option.label === newLabel)) {
-        new Notice(
-          t(
-            "column_settings_modal_selected_column_options_notice_error_duplicate_label"
-          ),
-          1500
-        );
-        return;
-      }
-      // Add new label
-      const newOption: RowSelectOption = {
-        label: newLabel,
-        backgroundColor: randomColor(),
-      };
-      options.push(newOption);
-      // Persist changes
-      view.diskConfig.updateColumnProperties(column.id, {
-        options: options,
-      });
-
-      this.addOptionSetting(
-        containerEl,
-        newOption,
-        options,
-        options.length - 1,
-        columnHandlerResponse
-      );
-    };
-    add_setting_header(containerEl, this.settingTitle, "h4");
-    new Setting(containerEl)
-      .setName(t("column_settings_modal_selected_column_options_new_option"))
-      .setDesc(
-        t("column_settings_modal_selected_column_options_new_option_desc")
-      )
-      .addText((text) => {
-        text
-          .setPlaceholder(
-            t(
-              "column_settings_modal_selected_column_options_new_option_placeholder"
-            )
-          )
-          .setValue(newLabel)
-          .onChange(async (value: string): Promise<void> => {
-            newLabel = satinizedColumnOption(value);
-            text.setValue(newLabel);
-          });
-        text.inputEl.onkeydown = (e: KeyboardEvent) => {
-          switch (e.key) {
-            case "Enter":
-              text.setValue("");
-              addLabelPromise();
-              break;
-          }
-        };
-      })
-      .addButton((button: ButtonComponent) => {
-        button
-          .setTooltip(
-            t(
-              "column_settings_modal_selected_column_options_new_option_button_tooltip"
-            )
-          )
-          .setButtonText("+")
-          .setCta()
-          .onClick(addLabelPromise);
-      });
 
     options.forEach((option, index) => {
       this.addOptionSetting(
